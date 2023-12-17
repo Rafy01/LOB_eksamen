cc510ceff5f915b24da451d8fb3be420a9a3c19b
DeanMileli <deanmileli@gmail.com>
2023-12-17 22:37:59 +0100

    rettelser

--- components/script.js
+++ components/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
-    // Handle hover effect for logo image
-    $('.icon-link').hover(function() {
-      // Hover-in: Change image src to the one with the gradient
-      $(this).find('.logo-hover').attr('src', $(this).find('.logo-hover').data('hover-src'));
-    }, function() {
-      // Hover-out: Change image src back to the default image
-      $(this).find('.logo-hover').attr('src', $(this).find('.logo-hover').data('default-src'));
-    });
+$(document).ready(function () {
+  // Handle hover effect for logo image
+  $('.icon-link').hover(function () {
+    // Hover-in: Change image src to the one with the gradient
+    $(this).find('.logo-hover').attr('src', $(this).find('.logo-hover').data('hover-src'));
+  }, function () {
+    // Hover-out: Change image src back to the default image
+    $(this).find('.logo-hover').attr('src', $(this).find('.logo-hover').data('default-src'));
+  });
 
-    // Handle hover effect for other elements (icon-image)
-    $('.icon-link .icon-image').hover(function() {
-      // Hover-in: Change image src to the one with the gradient
-      $(this).attr('src', $(this).data('hover-src'));
-    }, function() {
-      // Hover-out: Change image src back to the default image
-      $(this).attr('src', $(this).data('default-src'));
-    });
+  // Handle hover effect for other elements (icon-image)
+  $('.icon-link .icon-image').hover(function () {
+    // Hover-in: Change image src to the one with the gradient
+    $(this).attr('src', $(this).data('hover-src'));
+  }, function () {
+    // Hover-out: Change image src back to the default image
+    $(this).attr('src', $(this).data('default-src'));
+  });
 
-    // Plain JavaScript for hover effect on links
-    const links = document.querySelectorAll('.custom-link');
-    links.forEach(link => {
-      link.addEventListener('mouseenter', () => {
-        link.classList.add('gradient-hover');
-      });
-      link.addEventListener('mouseleave', () => {
-        link.classList.remove('gradient-hover');
-      });
+  // Plain JavaScript for hover effect on links
+  const links = document.querySelectorAll('.custom-link');
+  links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+      link.classList.add('gradient-hover');
+    });
+    link.addEventListener('mouseleave', () => {
+      link.classList.remove('gradient-hover');
     });
   });
+});
